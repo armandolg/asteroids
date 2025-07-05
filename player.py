657f2889572216869d39898631e9b418e1a16d7f
Armando Linares <armandolinaresg@gmail.com>
2025-07-04 18:32:50 -0600

    add turbo mode

--- player.py
+++ player.py
@@ -39,10 +39,16 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+        if keys[pygame.K_LSHIFT] and keys[pygame.K_w]:
+            self.turbo(dt)
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def turbo(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED_TURBO * dt
 
     def shoot(self):
         if self.shoot_timer < 0:
